be643b6859c31028cf5a2eae5ebb0f251c90515a
Oyster Su <jenhausu@icloud.com>
2019-12-03 12:40:06 +0800

    remove unused code

--- OneQuoteADay/HTTPResponse.swift
+++ OneQuoteADay/HTTPResponse.swift
@@ -8,20 +8,6 @@
 
 import Foundation
 
-let decoder = JSONDecoder()
-
 enum ResponseError: Error {
     case nilData
-}
-
-struct HTTPResponse<T: Codable> {
-    let value: T?
-    let response: HTTPURLResponse?
-    let error: Error?
-    
-    init(data: Data?, response: URLResponse?, error: Error?) throws {
-        self.value = try data.map{ try decoder.decode(T.self, from: $0) }
-        self.response = response as? HTTPURLResponse
-        self.error = error
-    }
 }
