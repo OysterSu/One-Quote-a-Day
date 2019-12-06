9ddbd0703724b0c8863f131b3d2231eabd29a4b5
蘇健豪 <jenhausu@icloud.com>
2019-12-06 21:20:39 +0800

    URLQuery implement

--- OneQuoteADay/NetworkRequest/Request/URLQueryDataAdapter.swift
+++ OneQuoteADay/NetworkRequest/Request/URLQueryDataAdapter.swift
@@ -12,6 +12,15 @@ struct URLQueryDataAdapter: RequestAdapter {
     let data: [String: Any]
     
     func adapted(_ request: URLRequest) -> URLRequest {
+        var request = request
+        var urlString = request.url!.absoluteString
+        urlString.append("?")
+        
+        var parms = data.compactMap { "\($0.key)=\($0.value)" }.joined(separator: "&")
+        parms = parms.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
+        
+        urlString.append(contentsOf: parms)
+        request.url = URL(string: urlString)
         
         return request
     }
